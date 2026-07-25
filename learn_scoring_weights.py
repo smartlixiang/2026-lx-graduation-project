8bcdd249bb839435d0693b7a31ee18946181bd80
Li Xiang <2945264435@qq.com>
2026-07-25 03:32:50 +0000

    xxx

--- learn_scoring_weights.py
+++ learn_scoring_weights.py
@@ -95,7 +95,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--ratio-lambda", type=float, default=5e-3)
     parser.add_argument("--regression-learning-rate", type=float, default=2e-3)
     parser.add_argument("--regression-max-iter", type=int, default=10000)
-    parser.add_argument("--regression-tol", type=float, default=1e-8)
+    parser.add_argument("--regression-tol", type=float, default=1e-6)
     parser.add_argument("--output", type=str, default="weights/scoring_weights.json")
     parser.add_argument("--device", type=str, default=None)
     parser.add_argument("--debug-prompts", action="store_true")
